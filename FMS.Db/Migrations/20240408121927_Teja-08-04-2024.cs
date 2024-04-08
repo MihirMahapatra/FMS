7cd4e28f574bb79b1b5f40f062a2f01f14db3a3e
MihirMohapatra <horizonsolutionsoffice@gmail.com>
2024-04-08 17:50:36 +0530

    commit

---
+++ FMS.Db/Migrations/20240408121927_Teja-08-04-2024.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FMS.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Teja08042024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMode",
+                schema: "dbo",
+                table: "SalesReturnOrders",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionNo",
+                schema: "dbo",
+                table: "Payments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMode",
+                schema: "dbo",
+                table: "SalesReturnOrders");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionNo",
+                schema: "dbo",
+                table: "Payments");
+        }
+    }
+}
