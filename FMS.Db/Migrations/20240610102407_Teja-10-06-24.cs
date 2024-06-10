cfe1f65bd0e1bec6663cdafe5b57dddc3468cc5d
MihirMohapatra <horizonsolutionsoffice@gmail.com>
2024-06-10 16:00:08 +0530

    commit

---
+++ FMS.Db/Migrations/20240610102407_Teja-10-06-24.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FMS.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Teja100624 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReceivingPerson",
+                schema: "dbo",
+                table: "PurchaseReturnOrders",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReceivingPerson",
+                schema: "dbo",
+                table: "PurchaseReturnOrders",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
